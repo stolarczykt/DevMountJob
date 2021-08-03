ef1e404cb9dfcf9b8d5deedf85151e66e446ae96
Tomek Stolarczyk <stolarczykt@gmail.com>
2021-08-03 08:45:10 +0200

    Randomness in test data.

--- advertisements/test/change_advertisements_content_test.rb
+++ advertisements/test/change_advertisements_content_test.rb
@@ -1,30 +1,29 @@
 require_relative 'test_helper'
+require 'securerandom'
 
 module Advertisements
   class ChangeAdvertisementsContentTest < ActiveSupport::TestCase
+    command_bus = Rails.configuration.command_bus
 
     test 'change advertisement content' do
-      advertisement_id = 68456
-      author_id = 7382
-      stream = "Advertisement$#{advertisement_id}"
-      new_content = "Astonishing new content!!!"
-      command_bus = Rails.configuration.command_bus
+      advertisement_id = SecureRandom.random_number
+      author_id = SecureRandom.random_number
+      new_content = "Random content: #{SecureRandom.hex}"
       command_bus.(PublishAdvertisement.new(advertisement_id, author_id))
 
       assert_events(
-          stream,
-          ContentHasChanged.new(data: {content: new_content})
+          stream_name = "Advertisement$#{advertisement_id}",
+          expected_events = ContentHasChanged.new(data: {content: new_content})
       ) do
         command_bus.(ChangeContent.new(advertisement_id, new_content, author_id))
       end
     end
 
     test 'do not allow changing content if not an author' do
-      advertisement_id = 68456
-      author_id = 7382
-      not_an_author_id = 5456456
-      new_content = "Astonishing new content!!!"
-      command_bus = Rails.configuration.command_bus
+      advertisement_id = SecureRandom.random_number
+      author_id = SecureRandom.random_number
+      not_an_author_id = SecureRandom.random_number
+      new_content = "Random content: #{SecureRandom.hex}"
       command_bus.(PublishAdvertisement.new(advertisement_id, author_id))
 
       assert_raises(Advertisement::NotAnAuthorOfAdvertisement) do
